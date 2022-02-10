626eda6ef5dc65c2e7c3eb8ca15633871bf2de2c
tzbodula <tzbodula@gmail.com>
2022-02-10 11:34:01 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./react-components/Header.js";
+import Footer from "./react-components/Footer.js";
+import Home from "./screens/home.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div>
+        <Home />
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HelmetProvider } from 'react-helmet-async';
+import App from "./App.js";
+import GalaxyMerch from "./screens/galaxymerch"
+import ProductScreen from "./screens/productscreen"
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StoreProvider } from "./Store.js";
+import CartScreen from "./screens/CartScreen.js";
+import LoginScreen from "./screens/Login.js";
+import ErrorBoundary from "./react-components/ErrorBoundary.js"
+
+console.log("Index Got Called!");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <StoreProvider>
+        <HelmetProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<LoginScreen />} />
+              <Route path="/galaxymerch" element={<GalaxyMerch />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </HelmetProvider>
+      </StoreProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
